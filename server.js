030f20c5c7abee3db72b14b145be13aca8bd7d93
almogc95 <117469540+almogc95@users.noreply.github.com>
2024-05-28 19:46:11 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: false }));
+
+const PORT = 3000;
+
+app.listen(PORT, () => console.log(`Listen on port ${PORT}`));
+
+app.get('/giveAndTake', (req, res) => {
+    res.render('giveAndTake');
+});
+
+app.get('/', (req, res) => {
+    res.render('homePage');
+});
+
+
+app.get('/chats', (req, res) => {
+    res.render('chats');
+});
+
+
+app.get('/donate', (req, res) => {
+    res.render('donate');
+});
+
+
+app.get('/about', (req, res) => {
+    res.render('about');
+});
+
