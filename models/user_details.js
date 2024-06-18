dd23fe0cd4658f2ea9f1608b2d45ed30a78b794c
Yehuda <yehudab105@gmail.com>
2024-06-18 19:08:50 +0300

    A little bit more from schema.
    Co-authored-by: shmueleden928@gmail.com <shmueleden928@gmail.com>

--- models/user_details.js
+++ models/user_details.js
@@ -1,11 +1,14 @@
+const mongoose = require('mongoose');
+
+// סכמה של הפתקים
 const notes_schema = new mongoose.Schema({
     give: {
-        type: String, required: [true, 'Give suggestion is required']
+        type: String, required: [true]
     },
     take: {
 
     },
-    location: {
+    date: {
         
     },
     time: {
@@ -13,28 +16,49 @@ const notes_schema = new mongoose.Schema({
     },
     notes: {
         
-    }
-})
+    },
+    location: {
+        
+    },
+});
 
 
 
 
 
 
-
-
+// מפתח ID של התופס
 
 const user_schema= new mongoose.Schema({
     
-    meil: {
-        type: String, required: [true, 'meil is required'],
+    name: {
+        type: String, required: [true, 'NAME is required'],
         validate: {
             validator: function (v) {
                 return (v.length >= 1 && v.trim() != '')
             },
             message: 'Name must contain at least one non-blank character'
         }
+    },
+    
+
+
+
+    mail: {
+        type: String, required: [true, 'mשil is required'],
+        validate: {
+            validator: function (v) {
+                return (v.length >= 1 && v.trim() != '')
+            },
+            message: 'Must put meul'
+        }
 },
+
+
+
+
+
+
 
 password: {
     type: String, required: [true, 'password is required'],
