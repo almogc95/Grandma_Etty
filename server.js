850448e7982c4dfa6eedcc92cdbdc969bbadd147
israel <israeleden720@gmail.com>
2024-06-25 14:26:52 +0300

    change server routes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
-user_details = mongoose.createConnection('mongodb://localhost:27017/user_details');
+user_details = mongoose.createConnection('mongodb://localhost:27017/grandma_etty_DB');
 give_and_take = mongoose.createConnection('mongodb://localhost:27017/give_and_take');
 const PORT = 3000;
 const projectRouter = require('./routes/project_router');
@@ -9,13 +9,12 @@ const projectRouter = require('./routes/project_router');
 const path = require('path');
 
 
-app.use('/grandmaEtty', projectRouter);
+
 app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/public',express.static(path.join(__dirname, '/public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-
+app.use('/grandmaEtty', projectRouter);
 
 
 app.listen(PORT, () => console.log(`Listen on port ${PORT}`));
