7660072eaa8cced6079a0e8e7aecc8439c3bc5c7
Yehuda <yehudab105@gmail.com>
2024-07-09 20:26:04 +0300

    התקנו את הסכמה של הפתקים על הסכמה של הגוגל Co-authored-by: shmueleden928@gmail.com <shmueleden928@gmail.com> Co-authored-by: almogc95 <almogc95@users.noreply.github.com>

--- src/models/google_model.js
+++ src/models/google_model.js
@@ -1,9 +1,46 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+
+// סכמה של הפתקים
+const notes_schema = new Schema({
+    // מה המשתמש נותן
+    give: {
+        type: String,
+
+    },
+
+    // מה המשתמש לוקח
+    take: {
+        type: String,
+    },
+
+    // תחילת הפגישה
+    Date_time: {
+        type: Date,
+    },
+
+    // סיום הפגישה
+    until: {
+        type: Date,
+    },
+
+    // מיקום הפגישה
+    location: {
+        type: String,
+    },
+
+    // ...הערות
+    notes: {
+        type: String,
+    }
+});
+
+
 const user_google_schema = new Schema({
     googleId: String,
     displayName: String,
-    email: String
+    email: String,
+    messages: [notes_schema]
 });
 
 const UserModel = mongoose.model('user_google_details', user_google_schema);
